07c59995f03c143e528fd90d36334092d68c2429
305 <305@305-PC>
2020-04-20 12:08:05 +0900

    ex02

--- 20200420/src/Ex02.java
+++ 20200420/src/Ex02.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class Ex02 {
 	public static void main(String[] args) {
-		System.out.println("스위치 공부해야됨");
+		System.out.println("aaa");
+
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		// ctrl + shift + f auto align
+		switch (input) {
+		case 1:
+			System.out.println("1input");			break;
+		case 2:
+			System.out.println("2input");
+			break;
+		case 3:
+			System.out.println("3input");
+			break;
+		default :
+			System.out.println("other");
+			break;
+		}
 	}
 }
